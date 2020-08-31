f286da5bb6684b8d99827092b87a9cd8d20df9a9
gunner-27 <user@Simple-mac.local>
2020-08-31 14:25:52 +0300

    feat(app): basics usage examples

---
+++ app/XlsxPopulate-export.js
@@ -0,0 +1,12 @@
+const XlsxPopulate = require("xlsx-populate");
+
+// Load a new blank workbook
+XlsxPopulate.fromBlankAsync().then((workbook) => {
+  // Modify the workbook.
+  workbook.sheet("Sheet1").cell("A1").value("This is neat!");
+
+  // Write to file.
+  return workbook.toFileAsync(
+    `./samples/XlsxPopulate-example-${new Date().toISOString()}.xlsx`
+  );
+});
